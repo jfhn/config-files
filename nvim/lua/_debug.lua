895152acf5f07bc8da0d3811b39cbc51635c16ed
Jan Hindges <jan.hindges@mni.thm.de>
2022-11-02 17:55:17 +0100

    Update debug library for neovim config.

--- nvim/lua/_debug.lua
+++ nvim/lua/_debug.lua
@@ -19,6 +19,9 @@ local function debug_print_function(func)
 	printf("function %s(), %s: %s", debug_get_func_name(func) or "<anonymous>", info.short_src, info.linedefined);
 end
 
+--- @param val          any      The value to be printed
+--- @param recursive    boolean? When `val` is a table, should its content be printed recursively? Default = `true`.
+--- @param indent_level number?  Level of indentation. Default = `0`.
 local function debug_print(val, recursive, indent_level)
 	if type(val) ~= "table" and type(val) ~= "function" then
 		print(tostring(val));
@@ -30,26 +33,27 @@ local function debug_print(val, recursive, indent_level)
 		return;
 	end
 
-	recursive    = recursive or false;
 	indent_level = indent_level or 0;
+	if recursive == nil then
+		recursive = true;
+	end
+
 	local indent_str = string.rep("\t", indent_level);
 	print(indent_str .. "{");
-	for k, v in pairs(val) do
-		local inner_indent_str = string.rep("    ", indent_level + 1);
-		cprint(inner_indent_str .. k .. " = ");
-		if type(v) == "table" then
-			print("");
+	if recursive then
+		for k, v in pairs(val) do
+			local inner_indent_str = string.rep("\t", indent_level + 1);
+			cprint(inner_indent_str .. k .. " = ");
+			if type(v) == "table" then
+
+				print("");
+			end
 			debug_print(v, recursive, indent_level + 1);
-		elseif type(v) == "function" then
-			debug_print_function(v);
-		else
-			print(tostring(v));
 		end
 	end
+
 	print(indent_str .. "}");
 end
-
-print("module loaded: _debug");
 
 return {
 	print = debug_print
--- nvim/lua/test.lua
+++ nvim/lua/test.lua
@@ -1,5 +1,12 @@
-test_func = function()
-	print("test func called");
-end
+local mydbg = require("_debug");
 
-print("test module loaded");
+local t = {
+	{x = 5, tab = {y = 5}},
+	abc = {1,2,3},
+	f = function () end
+};
+
+mydbg.print(t);
+mydbg.print(t, true);
+mydbg.print(t, false);
+mydbg.print(function() end);
