1acbe534ba153daca9d8236ee6afe2884a0bcdb0
Jan Hindges <jan.hindges@mni.thm.de>
2023-04-08 08:58:54 +0200

    Add missing init.lua file.

---
+++ nvim/lua/init.lua
@@ -0,0 +1,19 @@
+--- @param mode "n"|"i"|"v"
+--- @param keys string
+--- @param action string
+--- @param opts table|nil
+map_key = function(mode, keys, action, opts)
+	local options = {noremap = true};
+	if opts then
+		options = vim.tbl_extend("force", options, opts);
+	end
+	vim.api.nvim_set_keymap(mode, keys, action, options);
+end
+
+require("ui");
+require("editor");
+require("_cmp");
+require("lsp");
+require("telescope-config");
+require("git-config");
+require("_nvim-tree");
