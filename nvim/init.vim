8ecfa04996e2607bff8643a2917f76fa86085141
Jan Hindges <jan.hindges@mni.thm.de>
2023-04-16 21:51:33 +0200

    Update neovim config.

--- nvim/init.vim
+++ nvim/init.vim
@@ -12,7 +12,7 @@ if exists("g:neovide")
 	source <sfile>:h/neovide.vim
 endif
 
-lua BGTransparency(0);
+TBT " Toggle background transparency
 
 fun! HighlightNotes()
 	syn match myNotes /\%(NOTE:\)/
--- nvim/lua/editor.lua
+++ nvim/lua/editor.lua
@@ -1,9 +1,12 @@
-SetTabWidth = function(width)
+set_tab_width = function(width)
+	width = width and tonumber(width) or 4
 	vim.opt.tabstop = width
 	vim.opt.shiftwidth = width
 end
 
-SetTabWidth(4)
+set_tab_width(4)
+
+vim.cmd("command! -nargs=? SetTabWidth :lua set_tab_width(<args>)")
 
 vim.opt.listchars = {
 	tab = "| ",
@@ -12,20 +15,25 @@ vim.opt.listchars = {
 }
 vim.opt.list = true
 
+-- local transparent_groups = {"Normal", "SignColumn", "FoldColumn", "LineNr", "CursorLineNr"}
 local transparent_groups = {
 	"Normal",
 	"NonText",
 	"SignColumn"
 }
 
-BGTransparency = function(set_transparent)
-	if set_transparent == false then
+local is_transparent = false;
+vim.cmd("command! TBT :lua toggle_transparency()")
+
+toggle_transparency = function()
+	is_transparent = not is_transparent;
+
+	if is_transparent then
+		for _, group in ipairs(transparent_groups) do
+			vim.cmd(string.format("hi %s ctermbg=NONE guibg=NONE", group))
+		end
+	else
 		local current_theme = vim.api.nvim_exec("colorscheme", true)
 		vim.cmd("colorscheme " .. current_theme)
-		return
-	end
-
-	for _, group in ipairs(transparent_groups) do
-		vim.cmd(string.format("hi %s ctermbg=NONE guibg=NONE", group))
 	end
 end
