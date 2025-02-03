76e95a1bdd3375abb2ab41383fed10b190e0604c
Jan Hindges <jan.hindges@schmidt-embedded-systems.de>
2025-02-03 08:59:07 +0100

    [neovim] Update config

--- nvim2/lua/config/keymap.lua
+++ nvim2/lua/config/keymap.lua
@@ -1,2 +1,3 @@
 vim.keymap.set('n', '<leader>dl', vim.diagnostic.open_float);
 vim.keymap.set('n', '<leader>df', vim.diagnostic.setloclist);
+vim.keymap.set('n', '<leader>gr', vim.lsp.buf.references)
