fa7487c4019b38516f28d217b5d8521306a820ce
Jan Hindges <janfhindges@gmail.com>
2026-05-17 19:57:45 +0200

    [Neovim] update config

--- nvim3/init.lua
+++ nvim3/init.lua
@@ -10,9 +10,14 @@ vim.pack.add {
   'https://github.com/gelguy/wilder.nvim',
   'https://github.com/chomosuke/typst-preview.nvim',
   'https://github.com/vimpostor/vim-tpipeline',
+  'https://github.com/nvim-treesitter/nvim-treesitter',
+  { src = 'https://github.com/jfhn/nvim-vibe', version = 'plan/v2' },
 
   -- Git
   'https://github.com/NeogitOrg/neogit.git',
+  'https://github.com/tpope/vim-fugitive.git',
+  'https://github.com/tpope/vim-rhubarb.git',
+  'https://github.com/aaronhallaert/advanced-git-search.nvim',
 
   -- Themes
   'https://github.com/blazkowolf/gruber-darker.nvim',
@@ -20,8 +25,21 @@ vim.pack.add {
   'https://github.com/savq/melange-nvim',
   'https://github.com/rose-pine/neovim.git',
   'https://github.com/shaunsingh/nord.nvim',
+  'https://github.com/dgrco/deepwater.nvim',
+  'https://github.com/phha/zenburn.nvim',
+  'https://github.com/Shatur/neovim-ayu',
+  'https://github.com/maxmx03/solarized.nvim',
+  'https://github.com/ellisonleao/gruvbox.nvim',
+  'https://github.com/ydkulks/cursor-dark.nvim',
+
+  -- AI
+  'https://github.com/nickjvandyke/opencode.nvim',
+
+  -- Language Support
+  'https://github.com/Julian/lean.nvim',
 }
 
+require('nvim-vibe').setup {}
 require('oil').setup {
   default_file_explorer = true,
 
@@ -48,7 +66,6 @@ require('oil').setup {
     enabled = false,
     autosave_changes = false,
   },
-
   watch_for_changes = true,
   use_default_keymaps = true,
 
@@ -56,7 +73,6 @@ require('oil').setup {
     show_hidden = true
   },
 }
-require('neogit').setup {}
 local wilder = require('wilder')
 wilder.setup { modes = { ':', '/', '?' } }
 wilder.set_option('renderer', wilder.wildmenu_renderer {
@@ -67,8 +83,65 @@ require('typst-preview').setup {
     ['tinymist'] = '/home/jan/.local/bin/tinymist'
   }
 }
+require('telescope').setup {
+  extensions = {
+    advanced_git_search = {
+      {
+        -- Browse command to open commits in browser. Default fugitive GBrowse.
+        -- {commit_hash} is the placeholder for the commit hash.
+        browse_command = "GBrowse {commit_hash}",
+        -- when {commit_hash} is not provided, the commit will be appended to the specified command seperated by a space
+        -- browse_command = "GBrowse",
+        -- => both will result in calling `:GBrowse commit`
 
-vim.lsp.enable { 'lua_ls', 'pyright', 'tinymist', 'ts_ls', 'ols' }
+        -- fugitive or diffview
+        diff_plugin = "fugitive",
+        -- customize git in previewer
+        -- e.g. flags such as { "--no-pager" }, or { "-c", "delta.side-by-side=false" }
+        git_flags = {},
+        -- customize git diff in previewer
+        -- e.g. flags such as { "--raw" }
+        git_diff_flags = {},
+        git_log_flags = {},
+        -- Show builtin git pickers when executing "show_custom_functions" or :AdvancedGitSearch
+        show_builtin_git_pickers = false,
+        entry_default_author_or_date = "author", -- one of "author", "date" or "both"
+        keymaps = {
+          -- following keymaps can be overridden
+          toggle_date_author = "<C-w>",
+          open_commit_in_browser = "<C-o>",
+          copy_commit_hash = "<C-y>",
+          show_entire_commit = "<C-e>",
+        },
+
+        -- Telescope layout setup
+        telescope_theme = {
+          function_name_1 = {
+              -- Theme options
+          },
+          function_name_2 = "dropdown",
+          -- e.g. realistic example
+          show_custom_functions = {
+            layout_config = { width = 0.4, height = 0.4 },
+          },
+        }
+      }
+    }
+  }
+}
+require('lean').setup { mappings = true }
+require('nvim-treesitter').setup {
+  -- Directory to install parsers and queries to (prepended to `runtimepath` to have priority)
+  install_dir = vim.fn.stdpath('data') .. '/site'
+}
+require('nvim-treesitter').install { 'rust', 'typescript', 'javascript', 'lua', 'c', 'cpp', 'ocaml', 'haskell', 'python' }
+vim.api.nvim_create_autocmd('FileType', {
+  pattern = { 'python', 'lua', 'javascript' },
+  callback = function() vim.treesitter.start() end,
+})
+
+vim.lsp.config('*', {})
+vim.lsp.enable { 'lua_ls', 'pyright', 'tinymist', 'ts_ls', 'ols', 'rust-analyzer', 'hls' }
 
 vim.g.mapleader = ' '
 local map = vim.keymap.set
@@ -80,6 +153,8 @@ map('n', '<leader>b', ':Telescope buffers<CR>')
 map('n', '<leader>h', ':Telescope help_tags<CR>')
 map('n', '<leader>gg', ':Neogit<CR>')
 map('n', '<leader>e', ':Oil .<CR>')
+map('n', '<leader>vp', ':Telescope nvim-vibe<CR>')
+map('n', '<leader>vs', ':NvimVibe sidebar<CR>')
 
 vim.api.nvim_create_autocmd({ 'BufEnter', 'BufWinEnter' }, {
   pattern = { '*.js', '*.ts', '*.ml' },
@@ -112,6 +187,17 @@ vim.api.nvim_create_user_command('Make', Make, {
   desc = 'Updates make program if specified and executes it',
 })
 
-vim.cmd('source ' .. vim.fn.stdpath('config') .. '/minimal.vim')
-dofile(vim.fn.stdpath('config') .. '/local.lua')
-dofile(vim.fn.stdpath('config') .. '/trailing-whitespace.lua').toggle_trim_on_save()
+-- OpenCode config
+vim.api.nvim_create_user_command('OpenCode', function()
+  require("opencode").toggle()
+end, {})
+vim.keymap.set('x', '<C-l>', function()
+  require("opencode").ask("@this: ")
+end, { desc = "Add selection to OpenCode prompt" })
+
+vim.defer_fn(function()
+  vim.cmd('source ' .. vim.fn.stdpath('config') .. '/minimal.vim')
+  dofile(vim.fn.stdpath('config') .. '/local.lua')
+  require('neogit').setup {}
+  pcall(dofile, vim.fn.stdpath('config') .. '/trailing-whitespace.lua')
+end, 0)
